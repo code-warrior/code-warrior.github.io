c98aa5ae7306173a5f48de4016686dec26d3f468
Roy A Vanegas <roy@thecodeeducators.com>
2017-04-24 08:20:49 -0400

    [JS] Add example of console.warn

---
+++ tutorials/javascript/console/warn/console.warn.js
@@ -0,0 +1,5 @@
+console.warn('These kinds of console messages are useful in situations where you ' +
+        'want to warn the user of a problem that is not fatal.');
+
+console.warn('You can also %c style these messages with CSS. ',
+        'background-color: blue; color: white; padding: 2px; border-radius: 4px;');
