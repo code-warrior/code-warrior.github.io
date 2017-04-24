df49e4cd9ad941e9abe94ff2a792275ed197728f
Roy A Vanegas <roy@thecodeeducators.com>
2017-04-24 08:58:17 -0400

    Update warn message

--- tutorials/javascript/console/warn/console.warn.js
+++ tutorials/javascript/console/warn/console.warn.js
@@ -1,5 +1,9 @@
-console.warn('These kinds of console messages are useful in situations where you ' +
-        'want to warn the user of a problem that is not fatal.');
+console.warn('%c console.warn %c messages are useful in situations where you want ' +
+        'to warn the user of a problem that is not fatal. Note the warning icon ' +
+        'on the left and the grey, right-facing triangle arrow. You can click the ' +
+        'grey arrow to see which line generated the warning.',
+        'background-color: rgb(245, 189, 0); padding: 2px; border-radius: 4px;',
+        'background-color: transparent; color: black; padding: 0; border-radius: 0;');
 
 console.warn('You can also %c style these messages with CSS. ',
         'background-color: blue; color: white; padding: 2px; border-radius: 4px;');
