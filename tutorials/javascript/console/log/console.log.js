658031a0533a51e19a09ea2a94f51fadb3416c77
Roy A Vanegas <roy@thecodeeducators.com>
2017-04-24 09:49:22 -0400

    [JS] Add console.log example

---
+++ tutorials/javascript/console/log/console.log.js
@@ -0,0 +1,9 @@
+console.log('%c console.log %c messages are useful for simple output. Note: ' +
+        'Unlike console.warn and console.error, console.log does not generate ' +
+        'an icon along the left margin, nor does it generate a grey, ' +
+        'right-facing triangle arrow.',
+        'background-color: blue; color: white; padding: 2px; border-radius: 4px;',
+        'background-color: transparent; color: black; padding: 0; border-radius: 0;');
+
+console.log('You can %c style these messages with CSS. ',
+        'background-color: blue; color: white; padding: 2px; border-radius: 4px;');
