f407d0cb7fd4df0ba97e13963065b80eecf53e20
Roy A Vanegas <roy@thecodeeducators.com>
2017-04-24 15:07:31 -0400

    [JS] Add console.error example

---
+++ tutorials/javascript/console/error/console.error.js
@@ -0,0 +1,9 @@
+console.error('%c console.error %c messages are useful in situations where you want ' +
+        'to generate fatal-level errors. Note the red error icon ' +
+        'on the left and the grey, right-facing triangle arrow. You can click the ' +
+        'grey arrow to see which line generated the error.',
+        'background-color: rgb(245, 189, 0); padding: 2px; border-radius: 4px;',
+        'background-color: transparent; color: black; padding: 0; border-radius: 0;');
+
+console.error('You can also %c style these messages with CSS. ',
+        'background-color: blue; color: white; padding: 2px; border-radius: 4px;');
