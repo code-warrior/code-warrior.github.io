d30aff32ad817bbc81ebd1c09f0455d749c05275
Roy A Vanegas <roy@thecodeeducators.com>
2017-04-26 00:18:05 -0400

    [JS] Add example of getElementById

---
+++ tutorials/javascript/dom-referencing/get-element-by-id/js/app.js
@@ -0,0 +1,6 @@
+/*jslint browser, es6, single, for, devel, this */
+/*global window */
+
+let message = document.getElementById('msg');
+
+message.innerHTML = 'The method <code>getElementById</code> establishes a reference to a DOM element whose ID attribute matches <code>getElementById</code>’s parameter. For example, <code>getElementById(\'msg\')</code> references the paragraph on this page whose ID is <code>msg</code>. This content is written into that paragraph via <code>getElementById</code>.';
