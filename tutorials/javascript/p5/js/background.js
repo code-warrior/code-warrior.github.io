962834d19a4364d894f5f5e45cf2658826c15747
Roy Vanegas <roy@thecodeeducators.com>
2018-02-21 14:20:27 -0500

    Add background() and createCanvas() P5 examples

---
+++ tutorials/javascript/p5/js/background.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const WIDTH = 640,
+    HEIGHT = 480,
+    BG_COLOR = `#c0ffee`;
+
+function setup() {
+    createCanvas(WIDTH, HEIGHT);
+}
+
+function draw() {
+    // Read more at https://p5js.org/reference/#/p5/background
+    background(BG_COLOR);
+}
---
+++ tutorials/javascript/p5/js/create-canvas.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const WIDTH = 640,
+    HEIGHT = 480;
+
+function setup() {
+    // Read more at https://p5js.org/reference/#/p5/createCanvas
+    createCanvas(WIDTH, HEIGHT);
+}
