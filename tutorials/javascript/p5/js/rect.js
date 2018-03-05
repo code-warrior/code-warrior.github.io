f364296522c392e0d7c430c8064e870788b381e0
Roy Vanegas <roy@thecodeeducators.com>
2018-03-05 09:56:43 -0500

    Add P5 example of the rect() function

---
+++ tutorials/javascript/p5/js/rect.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const WIDTH = 640,
+    HEIGHT = 480;
+
+function setup() {
+    createCanvas(WIDTH, HEIGHT);
+}
+
+function draw() {
+    // Read more at https://p5js.org/reference/#/p5/rect
+    rect(0, 0, (WIDTH/2), (HEIGHT/2));
+}
